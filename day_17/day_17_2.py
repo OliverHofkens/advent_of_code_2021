a5560808850225996e307cd909c3d3155828599e
Oliver Hofkens <oliver@gorilla.co>
2021-12-17 16:48:41 +0100

    Day 17; Puzzle 2 basic Python solution.

---
+++ day_17/day_17_2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+from typing import Tuple
+
+Vec2 = Tuple[int, int]
+
+
+def trace_probe(velocity: Vec2, target_x: Vec2, target_y: Vec2) -> bool:
+    """
+    Simulates the trajectory of the probe.
+    Returns:
+        True if the probe hit the target, otherwise False
+    """
+    pos = [0, 0]
+    drag = -1 if velocity[0] > 0 else 1
+
+    while True:
+        pos[0] += velocity[0]
+        pos[1] += velocity[1]
+        velocity = velocity[0] + drag if velocity[0] != 0 else 0, velocity[1] - 1
+
+        # Check if we missed the target:
+        if pos[1] < target_y[0]:
+            return False
+
+        # Check if we're in the target zone:
+        if target_x[0] <= pos[0] <= target_x[1] and (
+            target_y[0] <= pos[1] <= target_y[1]
+        ):
+            return True
+
+
+def parse_target(inp: str) -> tuple:
+    inp = inp[len("target area: ") :]
+    parts = [map(int, p[2:].split("..")) for p in inp.split(", ")]
+    return tuple(tuple(t) for t in parts)
+
+
+def main():
+    target_x, target_y = parse_target(sys.stdin.readline())
+
+    # Ensure we shoot in the right direction:
+    guess_x = range(0, target_x[1] + 1, 1 if target_x[0] > 0 else -1)
+    guess_y = range(target_y[0], 300)
+
+    hits = [trace_probe((x, y), target_x, target_y) for x in guess_x for y in guess_y]
+
+    print(sum(hits))
+
+
+if __name__ == "__main__":
+    main()
